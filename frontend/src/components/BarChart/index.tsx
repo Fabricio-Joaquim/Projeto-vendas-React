82a21f84a657687b2f65f975d581d441af27d884
Fabricio <fabriciojoaquim25@hotmail.com>
2021-09-12 23:06:20 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,4 +1,9 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
+import * as types from '../../types/types'
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
 export default function BarChart(){
     const options = {
         plotOptions: {
@@ -7,22 +12,35 @@ export default function BarChart(){
             }
         },
     };
-    
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
+    const [chartData, setChartData] = useState<types.CharData2>({
+        labels:{
+            categories: []},
+        series: [   
+            {name:"% Sucesso",
+            data: [],}
         ]
-    };
+    })
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/sucess-by-seller`).then((response) => {
+            const data = response.data as types.SaleSuccess[]
+            const myLabels = data.map((item) => item.sellerName);
+            const mySeries = data.map((item) => round(100*item.deals / item.visited, 1));
+            setChartData({
+                labels:{
+                    categories: myLabels},
+                series: [   
+                    {name:"% Sucesso",
+                    data: mySeries,}
+                ]
+            });
+        })
+    }, [])
+
     return(
         <Chart 
-        options={{...options,xaxis:mockData.labels}}
-        series={mockData.series}
+        options={{...options,xaxis:chartData.labels}}
+        series={chartData.series}
         type='bar'
         height = "240"
         />
--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -7,3 +7,22 @@ export type ChartData = {
     labels: string[],
     series: number[],
 }
+
+export type SaleSuccess = {
+    sellerName: string,
+    visited: number,
+    deals:number
+}
+
+export type SeriesData={
+   name:string,
+   data:number[], 
+}
+
+export type CharData2 ={
+    labels:{categories: string[]},
+    series: [   
+        {name:string,
+        data:number[]}
+    ];
+}
